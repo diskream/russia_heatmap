1693ac6b528c549f9e8a151ca7aa50c1ee526985
Nikolay <diskream77@yandex.ru>
2023-10-14 18:31:37 +0300

    refactoring

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import uvicorn
 from fastapi import FastAPI, Response
-from shapely import MultiPolygon
+from geopandas import GeoDataFrame
+from shapely import MultiPolygon, Polygon
 from shapely.affinity import translate
 from shapely.geometry import LineString
 from shapely.ops import split
@@ -17,42 +18,55 @@ app = FastAPI()
 matplotlib.use('agg')
 
 
-def shift_map(mymap, shift):
+def translate_geometry(polygon: Polygon, geometry_list: list[Polygon | MultiPolygon], shift: int) -> None:
+    """Translates polygon with shift and appends to geometry list.
+
+    :param polygon:
+    :param geometry_list:
+    :param shift:
+    """
+    min_x, *_ = polygon.bounds
+    if min_x >= shift:
+        geometry_list.append(translate(polygon, xoff=-180 - shift))
+    else:
+        geometry_list.append(translate(polygon, xoff=180 - shift))
+
+
+def shift_map(geo_dataframe: GeoDataFrame, shift: int) -> list[Polygon | MultiPolygon]:
+    """
+
+    :param geo_dataframe:
+    :param shift:
+    :return:
+    """
     shift -= 180
-    moved_map = []
-    splitted_map = []
+    moved_map: list[Polygon | MultiPolygon] = []
     border = LineString([(shift, 90), (shift, -90)])
-    for row in mymap["geometry"]:
-        splitted_map.append(split(row, border))
-    for element in splitted_map:
+
+    split_map = [split(row, border) for row in geo_dataframe["geometry"]]
+
+    for element in split_map:
         items = list(element.geoms)
         if len(items) == 1:
-            item = items[0]
-            minx, miny, maxx, maxy = item.bounds
-            if minx >= shift:
-                moved_map.append(translate(item, xoff=-180 - shift))
-            else:
-                moved_map.append(translate(item, xoff=180 - shift))
+            translate_geometry(next(iter(items)), moved_map, shift)
             continue
-        multipolygon = []
+
+        multipolygon: list[MultiPolygon] = []
+
         for item in items:
-            minx, miny, maxx, maxy = item.bounds
-            if minx >= shift:
-                multipolygon.append(translate(item, xoff=-180 - shift))
-            else:
-                multipolygon.append(translate(item, xoff=180 - shift))
+            translate_geometry(item, multipolygon, shift)
+
         moved_map.append(MultiPolygon(multipolygon))
+
     return moved_map
 
 
-def get_plot():
-    SHAPEFILE = r'F:\Programming\Julya\russia_heatmap\RF\admin_4.shp'
-    # Read shapefile using Geopandas
-    geo_df = gpd.read_file(SHAPEFILE)[["name_ru", "ref", "geometry"]]
+def get_data_for_plot() -> pd.DataFrame:
+    geo_df = gpd.read_file(r'RF/admin_4.shp')[["name_ru", "ref", "geometry"]]
 
     geo_df['geometry'] = shift_map(geo_df, 100)
 
-    colormap_data = pd.read_csv(r'F:\Programming\Julya\russia_heatmap\RF\colormap.csv')
+    colormap_data = pd.read_csv(r'RF/colormap.csv')
     colormap_data['Названия строк'] = colormap_data['Названия строк'].replace(
         {
             'Республика Тыва': 'Тыва',
@@ -61,32 +75,53 @@ def get_plot():
             'Чувашская Республика - Чувашия': "Чувашия"
         }
     )
+
     colormap_data['Процент'] = colormap_data['Процент'].str.replace("%", '').str.replace(',', '.').astype(float)
 
-    target_region_names = list(geo_df['name_ru'])
-    colormap_region_names = list(colormap_data['Названия строк'])
+    target_region_names: list[str] = list(geo_df['name_ru'])
+    colormap_region_names: list[str] = list(colormap_data['Названия строк'])
     target_region_names.sort()
     colormap_region_names.sort()
 
-    target_map = {target: next(iter(difflib.get_close_matches(target, colormap_region_names, cutoff=0.5)), None) for
-                  target in target_region_names}
-    geo_df['name_ru'].replace(target_map, inplace=True)
-    merged_df = pd.merge(left=geo_df, right=colormap_data, left_on='name_ru', right_on='Названия строк', how="right")
+    target_map = {
+        target: next(iter(difflib.get_close_matches(target, colormap_region_names, cutoff=0.5)), None)
+        for target in target_region_names
+    }
 
-    col = 'Процент'
-    source = 'Процент дел (?) по регионам Российской Федерации'
-    vmin = merged_df[col].min()
-    vmax = merged_df[col].max()
-    cmap = 'YlGnBu'
+    geo_df['name_ru'].replace(target_map, inplace=True)
+
+    return pd.merge(left=geo_df, right=colormap_data, left_on='name_ru', right_on='Названия строк', how="right")
+
+
+def get_plot():
+    df = get_data_for_plot()
+
+    target_colormap_column: str = 'Процент'
+    color_map: str = 'YlGnBu'
 
     fig, ax = plt.subplots(1, figsize=(30, 12))
-    merged_df.plot(column=col, ax=ax, edgecolor='0.8', linewidth=1, cmap=cmap)
-    ax.annotate(source, xy=(0.1, .08), xycoords='figure fraction', horizontalalignment='left',
-                verticalalignment='bottom', fontsize=10)
-    # Create colorbar as a legend
-    sm = plt.cm.ScalarMappable(norm=plt.Normalize(vmin=vmin, vmax=vmax), cmap=cmap)
-    cbaxes = fig.add_axes([0.85, 0.11, 0.01, 0.77])
-    fig.colorbar(sm, cax=cbaxes)
+
+    df.plot(column=target_colormap_column, ax=ax, edgecolor='0.8', linewidth=1, cmap=color_map)
+
+    ax.annotate(
+        'Процент дел (?) по регионам Российской Федерации',
+        xy=(0.1, .08),
+        xycoords='figure fraction',
+        horizontalalignment='left',
+        verticalalignment='bottom',
+        fontsize=10,
+    )
+
+    fig.colorbar(
+        plt.cm.ScalarMappable(
+            norm=plt.Normalize(
+                vmin=df[target_colormap_column].min(),
+                vmax=df[target_colormap_column].max(),
+            ),
+            cmap=color_map,
+        ),
+        cax=fig.add_axes([0.85, 0.11, 0.01, 0.77]),
+    )
 
     plt.plot()
     img_buf = io.BytesIO()
