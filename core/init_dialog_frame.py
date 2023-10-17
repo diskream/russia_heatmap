5fb4c4108cefebf557c0fd769ab563c9c9731265
Nikolay <diskream77@yandex.ru>
2023-10-17 15:49:05 +0300

    pre-MVP

--- core/init_dialog_frame.py
+++ core/init_dialog_frame.py
@@ -1,62 +1,69 @@
+import difflib
+import logging
 import tkinter as tk
 from tkinter.filedialog import askopenfile
 from tkinter.ttk import Button, Frame, Label
 
-import matplotlib
-import matplotlib.pyplot as plt
+import geopandas as gpd
+import pandas as pd
 import typing_extensions
 
-from core.utils import get_data_for_plot
+from core.map_of_russia import RussiaHeatMap
+from core.utils import compile_gdf
 
 if typing_extensions.TYPE_CHECKING:
     from main import App
 
+logger = logging.getLogger("DialogFrame")
+
 
 class DialogFrame(Frame):
-    def __init__(self, master: "App"):
+    def __init__(self, master: "App", compiled_gdf_path: str = "map_data/russia_regions.parquet"):
         Frame.__init__(self, master)
 
         Label(self, text="Выберите .xlsx файл.").pack(side=tk.LEFT, padx=50, pady=5)
 
-        Button(self, text="Выбрать", command=self._get_xlsx_path_from_explorer).pack(side=tk.LEFT,padx=2, pady=5)
+        Button(self, text="Выбрать", command=self._get_xlsx_path_from_explorer).pack(side=tk.LEFT, padx=2, pady=5)
 
-    def _get_plot(self, file_path: str):
-        matplotlib.use("TkAgg")
+        try:
+            logger.info("Trying to load parquet {path}".format(path=compiled_gdf_path))
+            gdf = gpd.read_parquet(compiled_gdf_path)
+        except Exception as exc:
+            logger.error("Unable to load parquet {exc}; recompiling gdf...".format(exc=exc))
+            gdf = compile_gdf(compiled_gdf_path)
 
-        df = get_data_for_plot(file_path)
-        df.dropna(inplace=True)
+        self.gdf: gpd.GeoDataFrame = gdf
 
-        target_colormap_column: str = "Процент"
-        color_map: str = "YlGnBu"
+    def _get_heatmap_plot(self, path_to_colormap_xlsx: str):
+        colormap_data = pd.read_excel(path_to_colormap_xlsx)
 
-        fig, ax = plt.subplots(1, figsize=(30, 12))
+        target_region_names: list[str] = list(self.gdf["region"])
+        colormap_region_names: list[str] = list(colormap_data["Названия строк"])
+        target_region_names.sort()
+        colormap_region_names.sort()
 
-        df.plot(column=target_colormap_column, ax=ax, edgecolor="0.8", linewidth=1, cmap=color_map)
+        target_map: dict[str, str] = {
+            target: next(iter(difflib.get_close_matches(target, colormap_region_names, cutoff=0.5)), None)
+            for target in target_region_names
+        }
+        target_map["Севастополь"] = "Севастополь"
+        #
+        self.gdf["region"].replace(target_map, inplace=True)
 
-        df.apply(lambda x: ax.annotate(text=x["Названия строк"], xy=x.geometry.centroid.coords[0], ha='center'), axis=1)
+        new_df = pd.merge(
+            left=self.gdf, right=colormap_data, left_on="region", right_on="Названия строк", how="outer"
+        ).dropna()
 
-        ax.annotate(
-            "Процент дел (?) по регионам Российской Федерации",
-            xy=(0.1, .08),
-            xycoords="figure fraction",
-            horizontalalignment="left",
-            verticalalignment="bottom",
-            fontsize=10,
-        )
+        # TODO
+        new_df.loc[new_df.region == "Севастополь", "Процент"] = new_df.loc[
+            new_df.region == "Республика Крым", "Процент"
+        ]
 
-        fig.colorbar(
-            plt.cm.ScalarMappable(
-                norm=plt.Normalize(
-                    vmin=df[target_colormap_column].min(),
-                    vmax=df[target_colormap_column].max(),
-                ),
-                cmap=color_map,
-            ),
-            cax=fig.add_axes([0.85, 0.11, 0.01, 0.77]),
-        )
+        heatmap = RussiaHeatMap(gdf=new_df)
 
-        plt.show()
+        heatmap.update_layout(showlegend=False, dragmode="pan")
+        heatmap.show()
 
     def _get_xlsx_path_from_explorer(self):
         file = askopenfile(mode="r")
-        self._get_plot(file.name)
+        self._get_heatmap_plot(file.name)
--- core/utils.py
+++ core/utils.py
@@ -1,68 +1,33 @@
 import difflib
+from typing import Literal
 
 import geopandas as gpd
+import numpy as np
 import pandas as pd
-from geopandas import GeoDataFrame
+from matplotlib.colors import LinearSegmentedColormap
 from shapely import MultiPolygon, Polygon
-from shapely.affinity import translate
-from shapely.geometry import LineString
-from shapely.ops import split
+from shapely.geometry.base import GeometrySequence
+from shapely.ops import snap, unary_union
+from tqdm import tqdm
 
 
-def translate_geometry(polygon: Polygon, geometry_list: list[Polygon | MultiPolygon], shift: int) -> None:
-    """Translates polygon with shift and appends to geometry list.
+def fix_yamalo_nenestky_ao(gdf):
+    polygons: list[Polygon] = []
+    geoms: GeometrySequence = gdf.loc[gdf.region == "Чукотский автономный округ", "geometry"].values[0].geoms
 
-    :param polygon:
-    :param geometry_list:
-    :param shift:
-    """
-    min_x, *_ = polygon.bounds
-    if min_x >= shift:
-        geometry_list.append(translate(polygon, xoff=-180 - shift))
-    else:
-        geometry_list.append(translate(polygon, xoff=180 - shift))
+    for polygon in geoms:
+        polygons.extend([snap(polygon, geoms[idx], 100) for idx, _ in enumerate(geoms)])
 
+    gdf.loc[gdf.region == "Чукотский автономный округ", "geometry"] = unary_union(MultiPolygon(polygons))
 
-def shift_map(geo_dataframe: GeoDataFrame, shift: int) -> list[Polygon | MultiPolygon]:
-    """
-
-    :param geo_dataframe:
-    :param shift:
-    :return:
-    """
-    shift -= 180
-    moved_map: list[Polygon | MultiPolygon] = []
-    border = LineString([(shift, 90), (shift, -90)])
-
-    split_map = [split(row, border) for row in geo_dataframe["geometry"]]
-
-    for element in split_map:
-        items = list(element.geoms)
-        if len(items) == 1:
-            translate_geometry(next(iter(items)), moved_map, shift)
-            continue
-
-        multipolygon: list[MultiPolygon] = []
-
-        for item in items:
-            translate_geometry(item, multipolygon, shift)
-
-        moved_map.append(MultiPolygon(multipolygon))
-
-    return moved_map
+    return gdf
 
 
 def get_data_for_plot(local_data_file_path: str) -> pd.DataFrame:
     geo_df = gpd.read_file(r"RF/admin_4.shp")[["name_ru", "ref", "geometry"]]
 
-    polygons = []
-    for idx, polygon in enumerate(geo_df.loc[23]["geometry"].geoms):
-        if idx not in {0, 2, 3}:
-            polygons.append(polygon)
-            continue
-        polygons.append(translate(polygon, xoff=300 + 60))
-
-    geo_df["geometry"][23] = MultiPolygon(polygons)
+    # geo_df = geo_df.to_crs('EPSG:32646')
+    geo_df.geometry = geo_df.geometry.simplify(0.05)
 
     colormap_data = pd.read_excel(local_data_file_path)
     colormap_data["Названия строк"] = colormap_data["Названия строк"].replace(
@@ -70,10 +35,9 @@ def get_data_for_plot(local_data_file_path: str) -> pd.DataFrame:
             "Республика Тыва": "Тыва",
             "Удмуртская Республика": "Удмуртия",
             "Чеченская Республика": "Чечня",
-            "Чувашская Республика - Чувашия": "Чувашия"
+            "Чувашская Республика - Чувашия": "Чувашия",
         }
     )
-
 
     target_region_names: list[str] = list(geo_df["name_ru"])
     colormap_region_names: list[str] = list(colormap_data["Названия строк"])
@@ -88,3 +52,97 @@ def get_data_for_plot(local_data_file_path: str) -> pd.DataFrame:
     geo_df["name_ru"].replace(target_map, inplace=True)
 
     return pd.merge(left=geo_df, right=colormap_data, left_on="name_ru", right_on="Названия строк", how="right")
+
+
+def prepare_regions(gdf, area_threshold=100e6, simplify_tolerance=500):
+    """Подготовка регионов к построению
+
+    - Упрощение геометрии с допуском simplify_tol
+    - Удаление полигонов с площадью менее area_thr
+    """
+    gdf_ = gdf.copy()
+
+    # Вспомогательный столбец для упорядочивания регионов по площади
+    gdf_["area"] = gdf_.geometry.apply(lambda x: x.area)
+
+    # Удаляем маленькие полигоны
+    tqdm.pandas(desc="Удаление мелких полигонов")
+    gdf_.geometry = gdf_.geometry.progress_apply(
+        lambda geometry: MultiPolygon([p for p in geometry.geoms if p.area > area_threshold])
+        if type(geometry) == MultiPolygon
+        else geometry
+    )
+
+    # Упрощение геометрии
+    gdf_.geometry = gdf_.geometry.simplify(simplify_tolerance)
+
+    geoms = gdf_.geometry.values
+    pbar = tqdm(enumerate(geoms), total=len(geoms))
+    pbar.set_description_str("Объединение границ после упрощения")
+    # проходим по всем граничащим полигонам и объединяем границы
+    for i, g in pbar:
+        g1 = g
+        for g2 in geoms:
+            if g1.distance(g2) < 100:
+                g1 = snap(g1, g2, 800)
+        geoms[i] = g1
+    gdf_.geometry = geoms
+
+    # сортировка по площади
+    gdf_ = gdf_.sort_values(by="area", ascending=False).reset_index(drop=True)
+
+    return gdf_.drop(columns=["area"])
+
+
+def geom_to_shape(g):
+    """Преобразование полигонов и мультиполигонов в plotly-readable шэйпы
+
+    Получает на вход Polygon или MultiPolygon из geopandas,
+    возвращает pd.Series с координатами x и y
+    """
+    # Если мультиполигон, то преобразуем каждый полигон отдельно, разделяя их None'ами
+    if type(g) == MultiPolygon:
+        x, y = np.array([[], []])
+        for poly in g.geoms:
+            x_, y_ = poly.exterior.coords.xy
+            x, y = (np.append(x, x_), np.append(y, y_))
+            x, y = (np.append(x, None), np.append(y, None))
+        x, y = x[:-1], y[:-1]
+    # Если полигон, то просто извлекаем координаты
+    elif type(g) == Polygon:
+        x, y = np.array(g.exterior.coords.xy)
+    # Если что-то другое, то возвращаем пустые массивы
+    else:
+        x, y = np.array([[], []])
+    return pd.Series([x, y])
+
+
+def compile_gdf(path: str, mode: Literal["pickle", "parquet"] = "parquet"):
+    gdf = gpd.read_file("map_data/russia_regions.geojson")
+
+    gdf.to_crs("ESRI:102027", inplace=True)
+
+    gdf = fix_yamalo_nenestky_ao(gdf)
+
+    gdf = prepare_regions(gdf)
+
+    gdf[["x", "y"]] = gdf.geometry.progress_apply(geom_to_shape)
+
+    if mode == "pickle":
+        gdf.to_pickle(path)
+    else:
+        gdf.to_parquet(path)
+
+    return gdf
+
+
+def get_color_range(colormap: LinearSegmentedColormap, color_range: int, mode: Literal["rgb", "rgba"] = "rgb"):
+    h = 1.0 / (color_range - 1)
+    colorscale = []
+
+    for alpha in range(color_range):
+        color = list(map(np.uint8, np.array(colormap(alpha * h)[:3]) * 255))
+        rgb_a_tuple = tuple(*color) if mode == "rgb" else (*color, 0.9)
+        colorscale.append([alpha * h, mode + str(rgb_a_tuple)])
+
+    return colorscale
